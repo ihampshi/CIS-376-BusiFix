cc38b41e771368bab7fcefbd0fd7204200c8211c
ashuhait <56399252+ashuhait@users.noreply.github.com>
2020-10-27 14:54:56 -0400

    saving file

---
+++ Busifix/src/busifix/Busifix.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busifix;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
+
+public class Busifix extends Application{
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+                Parent root = FXMLLoader.load(getClass().getResource("BusifixFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Busifix");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ Busifix/src/busifix/BusifixFXMLController.java
@@ -0,0 +1,26 @@
+package busifix;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+
+public class BusifixFXMLController implements Initializable{
+
+    @FXML
+    private Button pressMeButton;
+
+    @FXML
+    void buttonAction(ActionEvent event) {
+
+    }
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }  
+}
+
